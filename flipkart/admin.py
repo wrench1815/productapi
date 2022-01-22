a403cc3268fd7344ea67419285c93e7788970133
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-01-22 19:42:28 +0530

    register models into admin

--- flipkart/admin.py
+++ flipkart/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    '''Admin View for Product'''
+
+    list_display = ('name', 'price')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    '''Admin View for Category'''
+
+    list_display = ('name', 'description')

55b5fc3d152dd00d2de44ace4563d44a9d4d0dc5
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-01-22 18:12:48 +0530

    add django jazzmin config

--- productapi/settings.py
+++ productapi/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -131,3 +132,117 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+# yapf: disable
+# Jazzmin settings
+JAZZMIN_SETTINGS = {
+    # title of the window (Will default to current_admin_site.site_title if absent or None)
+    'site_title': 'Whispyr Admin',
+    'navbar_small_text': False,
+
+    # Title on the brand, and login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    'site_header': 'Whispyr Admin',
+
+    # square logo to use for your site, must be present in static files, used for favicon and brand on top left
+    # 'site_logo': 'favicon.ico',
+
+    # Welcome text on the login screen
+    'welcome_sign': 'Need some Powers?',
+
+    # Copyright on the footer
+    'copyright': 'wrench1815',
+
+    ############
+    # Top Menu #
+    ############
+
+    # Links to put along the top menu
+    'topmenu_links': [
+
+        # Url that gets reversed (Permissions can be added)
+        {
+            'name': 'Home',
+            'url': 'admin:index',
+            'permissions': ['auth.view_user']
+        },
+
+        # model admin to link to (Permissions checked against model)
+        {
+            'model': 'auth.User'
+        },
+    ],
+
+    #############
+    # Side Menu #
+    #############
+
+    # Whether to display the side menu
+    'show_sidebar': True,
+
+    # Whether to auto expand the menu
+    'navigation_expanded': True,
+
+    # Hide these apps when generating side menu e.g (auth)
+    'hide_apps': [],
+
+    # Hide these models when generating side menu (e.g auth.user)
+    'hide_models': [],
+
+    # Just make sure auth is first
+    'order_with_respect_to': ['auth'],
+
+    #############
+    # UI Tweaks #
+    #############
+
+    # Whether to show the UI customizer on the sidebar
+    'show_ui_builder': False,
+
+
+    ###############
+    # Change view #
+    ###############
+    # Render out the change view as a single form, or in tabs, current options are
+    # - single
+    # - horizontal_tabs (default)
+    # - vertical_tabs
+    # - collapsible
+    # - carousel
+    'changeform_format': 'horizontal_tabs',
+
+    # Render django related popups inside a modal using
+    'related_modal_active': True
+}
+
+JAZZMIN_UI_TWEAKS={
+    'footer_small_text': False,
+    'body_small_text': False,
+    'brand_small_text': False,
+    'brand_colour': False,
+    'accent': 'accent-primary',
+    'navbar': 'navbar-white navbar-light',
+    'no_navbar_border': False,
+    'navbar_fixed': True,
+    'layout_boxed': False,
+    'footer_fixed': False,
+    'sidebar_fixed': True,
+    'sidebar': 'sidebar-light-primary',
+    'sidebar_nav_small_text': False,
+    'sidebar_disable_expand': False,
+    'sidebar_nav_child_indent': False,
+    'sidebar_nav_compact_style': False,
+    'sidebar_nav_legacy_style': False,
+    'sidebar_nav_flat_style': False,
+    'theme': 'default',
+    # 'dark_mode_theme': None,
+    'button_classes': {
+        'primary': 'btn-outline-primary',
+        'secondary': 'btn-outline-secondary',
+        'info': 'btn-outline-info',
+        'warning': 'btn-outline-warning',
+        'danger': 'btn-outline-danger',
+        'success': 'btn-outline-success'
+    },
+    'actions_sticky_top': True
+}
+# yapf: enable

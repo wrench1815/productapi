1fbb16d1f11bdee866ce297e5109e49c8ca183a6
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-01-24 14:06:24 +0530

    updated serializer to include related data

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,12 @@
 from django.db.models import fields
 from rest_framework import serializers
-from flipkart.models import Product, Category
+from flipkart.models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    '''Serializer for Product Model'''
 
     class Meta:
         model = Product
         fields = '__all__'
+        depth = 1

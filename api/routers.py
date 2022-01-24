77090637233f91a4e29e93076eea264069065aba
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-01-24 15:37:38 +0530

    add amazon to router
    refactor code

--- api/routers.py
+++ api/routers.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .viewsets import ProductViewSet as fProductViewSet
+from rest_framework import routers
+# from rest_framework.routers import DefaultRouter
+from .viewsets import FlipkartProductViewSet, AmazonProductViewSet
 
 # Creates a new Router
-router = DefaultRouter()
-router.register(r'products', fProductViewSet)
+router = routers.DefaultRouter()
+router.register(r'flipkart', FlipkartProductViewSet, basename='flipkart')
+router.register(r'amazon', AmazonProductViewSet, basename='amazon')

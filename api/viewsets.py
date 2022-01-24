dd49f694e278ae2a62e31b6294324aa3c4939648
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-01-24 15:37:19 +0530

    add viewsets for amazon

--- api/viewsets.py
+++ api/viewsets.py
@@ -1,8 +1,18 @@
 from rest_framework import viewsets
-from .serializers import ProductSerializer as fProductSerializer
+from .serializers import FlipkartProductSerializer, AmazonProductSerializer
+
+# flipkart imports
 from flipkart.models import Product as fproduct
 
+# amazon imports
+from amazon.models import Product as aproduct
 
-class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+
+class FlipkartProductViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = fproduct.objects.all()
-    serializer_class = fProductSerializer
+    serializer_class = FlipkartProductSerializer
+
+
+class AmazonProductViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = aproduct.objects.all()
+    serializer_class = AmazonProductSerializer

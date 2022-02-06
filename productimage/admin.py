653b4ec1b778a3ac827acd70d8b90b1becf6013f
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-02-06 13:08:56 +0530

    update admin view

--- productimage/admin.py
+++ productimage/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    '''Admin View for ProductImage'''
+
+    # list_display = ('url', )

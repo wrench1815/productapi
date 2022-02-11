6805ed864aaa68f2c0839642a278a6bfdacedf6a
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-02-11 12:22:59 +0530

    add pagination

--- amazon/viewsets.py
+++ amazon/viewsets.py
@@ -1,12 +1,20 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
 from . import models
 from . import serializers
 
 
+class CustomResultPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
 class AmazonMobileViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Mobile.objects.all()
     serializer_class = serializers.AmazonMobileSerializer
+    pagination_class = CustomResultPagination
 
 
 class AmazonBrandViewSet(viewsets.ReadOnlyModelViewSet):
--- flipkart/viewsets.py
+++ flipkart/viewsets.py
@@ -1,12 +1,20 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
 from . import models
 from . import serializers
 
 
+class CustomResultPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
 class FlipkartMobileViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Mobile.objects.all()
     serializer_class = serializers.FlipkartMobileSerializer
+    pagination_class = CustomResultPagination
 
 
 class FlipkartBrandViewSet(viewsets.ReadOnlyModelViewSet):
